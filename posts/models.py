5f4b753228f0af453d7697a4205a7fd326f8e30f
Nurmuhammedm <nurmuhammedmukaramov@alatoo.edu.kg>
2023-04-12 16:57:23 +0600

    models, commits

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model) :
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model) :
+         title = models.CharField(max_length=100)
+         content = models.TextField(blank=True)
+         #post_image
+         # autor
+         tags = models.ManyToManyField(Tag blank=true)
